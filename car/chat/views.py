39e69e5a104c2ea20506f429acb28b05ad7f60cf
= <=>
2018-08-27 20:17:32 +0800

    添加公众号查询汽车状态功能

--- car/chat/views.py
+++ car/chat/views.py
@@ -4,10 +4,15 @@ from wechatpy import parse_message,create_reply
 from wechatpy.exceptions import InvalidSignatureException
 from wechatpy.utils import check_signature
 from wechatpy import WeChatClient
-
+from cartest.models import Carinfo
 
 
 client = WeChatClient('wx65ecd82cb1ce0559','9c5acf854e9fe944657c775ced596e81')
+WECHAT_TOKEN = 'slh201008'
+
+'''
+自定义菜单借口
+'''
 client.menu.create(
         {
         'button':[
@@ -17,16 +22,17 @@ client.menu.create(
                 'key':'car_find'
                 },
         {
-                'type':'click',
+                'type':'view',
                 'name':'车辆订单',
-                'key':'car_dingdan'
+                'url':'http://119.29.160.42/cartest/index/'
         }
         ]
         }
 )
 
-
-WECHAT_TOKEN = 'slh201008'
+'''
+自动回复，车辆查询
+'''
 @csrf_exempt
 def weixin(request):
         if request.method == 'GET':
@@ -43,7 +49,21 @@ def weixin(request):
         elif request.method == 'POST':
                 msg = parse_message(request.body)
                 if msg.type =="text":
-                        reply = create_reply('A',msg)
+                        if Carinfo.objects.filter(car_number=msg.content):
+                                car_msg = Carinfo.objects.filter(car_number=msg.content)
+                                for p in car_msg:
+                                        car_state = p.car_state
+                                if car_state == 'dj':
+                                        car_state = "待检测"
+                                        reply = create_reply(car_state,msg)
+                                if car_state == 'jc':
+                                        car_state = "检测中"
+                                        reply = create_reply(car_state,msg)
+                                if car_state == 'yj':
+                                        car_state = "检测完毕"
+                                        reply = create_reply(car_state,msg)
+                        else:
+                                reply = create_reply('您输入的车辆不存在',msg)
                 elif msg.type == 'image':
                         reply = create_reply('B',msg)
                 elif msg.type == 'voice':
